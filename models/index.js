638969787b4a669d46a9338496d0156ae1588b61
kwlucas <lucaswkyle@gmail.com>
2022-11-18 12:17:07 -0700

    Set up model index

---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Game = require('./Game');
+const Character = require('./Character');
+const Statset = require('./Statset');
+
+
+module.exports = { User, Game, Character, Statset };
